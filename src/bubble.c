6df2110cbb9f3a17bdd1828093591bc9a4b9171d
TSEG <pcat2122@gmail.com>
2026-05-08 00:56:37 -0400

    Init

---
+++ src/bubble.c
@@ -0,0 +1,323 @@
+#include "modding.h"
+#include "global.h"
+#include "recomputils.h"
+#include "recompconfig.h"
+#include "z_en_bb.h"
+#include "overlays/actors/ovl_En_Clear_Tag/z_en_clear_tag.h"
+#include "assets/objects/gameplay_keep/gameplay_keep.h"
+
+int TimerBB = 0;
+bool CanUseTimerBB = false;
+bool finalStand = true;
+void EnBb_SetupFlyIdle(EnBb* this);
+void EnBb_Attack(EnBb* this, PlayState* play);
+void EnBb_Down(EnBb* this, PlayState* play);
+
+void EnBb_CheckForWall(EnBb* this) {
+    s16 yawDiff;
+
+    if (this->actor.bgCheckFlags & BGCHECKFLAG_WALL) {
+        yawDiff = this->actor.shape.rot.y - this->actor.wallYaw;
+        if (ABS_ALT(yawDiff) > 0x4000) {
+            this->actor.shape.rot.y = ((this->actor.wallYaw * 2) - this->actor.shape.rot.y) - 0x8000;
+        }
+
+        this->targetYRotation = this->actor.shape.rot.y;
+        this->actor.bgCheckFlags &= ~BGCHECKFLAG_WALL;
+    }
+}
+
+void EnBb_UpdateStateForFlying(EnBb* this) {
+    SkelAnime_Update(&this->skelAnime);
+    if (this->actor.floorHeight > BGCHECK_Y_MIN) {
+        Math_StepToF(&this->actor.world.pos.y, this->actor.floorHeight + this->flyHeightMod, 0.5f);
+    }
+
+    this->actor.world.pos.y += Math_CosS(this->bobPhase);
+    this->bobPhase += 0x826;
+    Math_StepToF(&this->flameScaleY, 0.8f, 0.1f);
+    Math_StepToF(&this->flameScaleX, 1.0f, 0.1f);
+    EnBb_CheckForWall(this);
+    Math_StepToF(&this->actor.speed, this->maxSpeed, 0.5f);
+    Math_ApproachS(&this->actor.shape.rot.y, this->targetYRotation, 5, 0x3E8);
+    this->actor.world.rot.y = this->actor.shape.rot.y;
+}
+
+RECOMP_HOOK_RETURN("EnBb_Init") void BBBuff(Actor* thisx, PlayState* play) {
+
+	EnBb* this = (EnBb*)thisx;
+	int Difficulty = (int)recomp_get_config_double("diff_option");
+    u8 baseHealth = this->actor.colChkInfo.health;
+
+    switch (Difficulty) {
+    case 1:
+        this->attackRange = 400.0f;
+        this->actor.colChkInfo.health = baseHealth * 2;
+        break;
+
+    case 2:
+        this->attackRange = 800.0f;
+        this->actor.colChkInfo.health = baseHealth * 3;
+        break;
+
+    default:
+        this->attackRange = 200.0f;
+        this->actor.colChkInfo.health = baseHealth;
+        break;
+    }
+
+    CanUseTimerBB = true;
+
+}
+
+RECOMP_HOOK("EnBb_SetupRevive") void Revival(EnBb* this) {
+
+    CanUseTimerBB = true;
+    finalStand = true;
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    u8 baseHealth = this->actor.colChkInfo.health;
+
+    switch (Difficulty) {
+    case 1:
+        this->actor.colChkInfo.health = baseHealth * 2;
+        break;
+
+    case 2:
+        this->actor.colChkInfo.health = baseHealth * 3;
+        break;
+
+    default:
+        this->actor.colChkInfo.health = baseHealth;
+        break;
+    }
+
+}
+
+RECOMP_HOOK("EnBb_Update") void Updating(Actor* thisx, PlayState* play) {
+
+    EnBb* this = (EnBb*)thisx;
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    u8 baseHealth = this->actor.colChkInfo.health;
+
+    if (Difficulty == 2) {
+
+        if (Rand_ZeroOne() < 0.05f) {
+
+            this->actor.home.pos.x =
+                this->actor.world.pos.x + Rand_CenteredFloat(300.0f);
+
+            this->actor.home.pos.z =
+                this->actor.world.pos.z + Rand_CenteredFloat(300.0f);
+
+            this->actor.home.pos.y =
+                this->actor.world.pos.y + Rand_CenteredFloat(50.0f);
+        }
+    }
+
+    if (CanUseTimerBB) {
+
+        TimerBB++;
+
+        if (TimerBB >= 5) {
+
+            switch (Difficulty) {
+            case 1:
+                this->attackRange = 350.0f;
+                this->actor.colChkInfo.health = baseHealth * 2;
+                break;
+
+            case 2:
+                this->attackRange = 500.0f;
+                this->actor.colChkInfo.health = baseHealth * 3;
+                break;
+
+            default:
+                this->attackRange = 200.0f;
+                this->actor.colChkInfo.health = baseHealth;
+                break;
+            }
+
+            TimerBB = 0;
+            CanUseTimerBB = false;
+        }
+    }
+}
+
+RECOMP_HOOK("EnBb_SetupWaitForRevive") void Vive(EnBb* this) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+
+    switch (Difficulty) {
+    case 1:
+        this->timer = 100;
+        break;
+
+    case 2:
+        this->timer = 50;
+        break;
+
+    default:
+        this->timer = 200;
+        break;
+    }
+}
+
+RECOMP_HOOK("EnBb_UpdateDamage") void damagestuff(EnBb* this, PlayState* play) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    int newJinx = 1200;
+
+    switch (Difficulty) {
+    case 1:
+        newJinx = 1400;
+        break;
+
+    case 2:
+        newJinx = 1800;
+        break;
+
+    default:
+        newJinx = 1200;
+        break;
+    }
+
+    if (gSaveContext.jinxTimer == 1200) {
+
+        gSaveContext.jinxTimer = newJinx;
+
+    }
+}
+
+RECOMP_HOOK("EnBb_SetupAttack") void IamSpeed(EnBb* this) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+
+    switch (Difficulty) {
+    case 1:
+        this->maxSpeed = Rand_ZeroFloat(2.0f) + 5.0f;
+        break;
+
+    case 2:
+        this->maxSpeed = Rand_ZeroFloat(3.0f) + 6.0f;
+        break;
+
+    default:
+        this->maxSpeed = Rand_ZeroFloat(1.5f) + 4.0f;
+        break;
+    }
+
+}
+
+RECOMP_PATCH void EnBb_SetupAttack(EnBb* this) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    float diffbasedMaxSpeed = 1.0f;
+
+    switch (Difficulty) {
+    case 1:
+        diffbasedMaxSpeed = 1.25f;
+        break;
+
+    case 2:
+        diffbasedMaxSpeed = 1.5f;
+        break;
+
+    default:
+        diffbasedMaxSpeed = 1.0f;
+        break;
+    }
+
+    Animation_PlayLoop(&this->skelAnime, &gBubbleAttackAnim);
+    this->timer = (s32)Rand_ZeroFloat(20.0f) + 60 * diffbasedMaxSpeed;
+    this->flyHeightMod = (Math_CosS(this->bobPhase) * 10.0f) + 30.0f;
+    this->targetYRotation = this->actor.yawTowardsPlayer;
+    this->maxSpeed = Rand_ZeroFloat(1.5f) + 4.0f * diffbasedMaxSpeed;
+    this->actionFunc = EnBb_Attack;
+}
+
+RECOMP_PATCH void EnBb_Attack(EnBb* this, PlayState* play) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    float sightDist = 120.f;
+    float chaseDist = 400.0f;
+
+    switch (Difficulty) {
+    case 1:
+        sightDist = 240.0f;
+        chaseDist = 600.0f;
+        break;
+
+    case 2:
+        sightDist = 360.0f;
+        chaseDist = 800.0f;
+        break;
+
+    default:
+        sightDist = 120.0f;
+        chaseDist = 400.0f;
+        break;
+    }
+
+    this->targetYRotation = this->actor.yawTowardsPlayer;
+    EnBb_UpdateStateForFlying(this);
+
+    if (Animation_OnFrame(&this->skelAnime, 0.0f) || Animation_OnFrame(&this->skelAnime, 5.0f)) {
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_MOUTH);
+    }
+    else if (Animation_OnFrame(&this->skelAnime, 2.0f) || Animation_OnFrame(&this->skelAnime, 7.0f)) {
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_WING);
+    }
+    else if (Animation_OnFrame(&this->skelAnime, 0.0f) && (Rand_ZeroOne() < 0.1f)) {
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_LAUGH);
+    }
+
+    this->timer--;
+
+    if (((this->attackRange + sightDist) < this->actor.xzDistToPlayer) || (this->timer == 0) ||
+        (Player_GetMask(play) == PLAYER_MASK_STONE) ||
+        (Actor_WorldDistXZToPoint(&this->actor, &this->actor.home.pos) > (chaseDist * 2))) {
+        EnBb_SetupFlyIdle(this);
+    }
+}
+
+RECOMP_PATCH void EnBb_FlyIdle(EnBb* this, PlayState* play) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    float sightDist = 120.f;
+
+    switch (Difficulty) {
+    case 1:
+        sightDist = 240.0f;
+        break;
+
+    case 2:
+        sightDist = 360.0f;
+        break;
+
+    default:
+        sightDist = 120.0f;
+        break;
+    }
+
+    EnBb_UpdateStateForFlying(this);
+
+    if (Animation_OnFrame(&this->skelAnime, 5.0f)) {
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_WING);
+    }
+    else if (Animation_OnFrame(&this->skelAnime, 0.0f) && (Rand_ZeroOne() < 0.1f)) {
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_LAUGH);
+    }
+
+    DECR(this->attackWaitTimer);
+    this->timer--;
+
+    if ((this->attackWaitTimer == 0) && (this->actor.xzDistToPlayer < this->attackRange + sightDist) &&
+        (Player_GetMask(play) != PLAYER_MASK_STONE)) {
+        EnBb_SetupAttack(this);
+    }
+    else if (this->timer == 0) {
+
+      if (Difficulty == 0)  EnBb_SetupFlyIdle(this);
+    }
+}
---
+++ src/slime.c
@@ -0,0 +1,239 @@
+#include "modding.h"
+#include "global.h"
+#include "recomputils.h"
+#include "recompconfig.h"
+#include "gameplay_keep.h"
+#include "overlays/actors/ovl_En_Clear_Tag/z_en_clear_tag.h"
+#include "z_en_slime.h"
+#include "assets/objects/gameplay_keep/gameplay_keep.h"
+
+#define ICE_BLOCK_TIMER_MAX 254
+#define ICE_BLOCK_UNUSED (ICE_BLOCK_TIMER_MAX + 1)
+
+void EnSlime_Damaged(EnSlime* this, PlayState* play);
+void EnSlime_Revive(EnSlime* this, PlayState* play);
+void EnSlime_SetupTurnToPlayer(EnSlime* this);
+void ReviveBuffFix(EnSlime* this);
+void EnSlime_Jump(EnSlime* this, PlayState* play);
+void EnSlime_SetupMoveInDirection(EnSlime* this);
+
+int Timer = 0;
+bool CanUseTimer = false;
+
+extern Gfx gItemDropDL[];
+
+AnimatedMaterial* sSlimeTexAnim;
+
+static TexturePtr sEyeTextures[] = {
+    gChuchuEyeOpenTex,
+    gChuchuEyeHalfTex,
+    gChuchuEyeClosedTex,
+    gChuchuEyeHalfTex,
+};
+
+static Vec3f sBodyPartPosOffsets[EN_SLIME_BODYPART_MAX] = {
+    { 2000.0f, 2000.0f, 0.0f },     // EN_SLIME_BODYPART_0
+    { -1500.0f, 2500.0f, -500.0f }, // EN_SLIME_BODYPART_1
+    { -500.0f, 1000.0f, 2500.0f },  // EN_SLIME_BODYPART_2
+    { 0.0f, 4000.0f, 0.0f },        // EN_SLIME_BODYPART_3
+    { 0.0f, 2000.0f, -2000.0f },    // EN_SLIME_BODYPART_4
+};
+
+typedef enum EnSlimeEyeTexture {
+    /* 0 */ EN_SLIME_EYETEX_OPEN,
+    /* 1 */ EN_SLIME_EYETEX_HALF,
+    /* 2 */ EN_SLIME_EYETEX_CLOSED,
+    /* 3 */ EN_SLIME_EYETEX_HALF2,
+    /* 4 */ EN_SLIME_EYETEX_MAX
+} EnSlimeEyeTexture;
+
+
+RECOMP_HOOK_RETURN("EnSlime_Init")
+void SlimeBuff(Actor* thisx, PlayState* play) {
+
+    EnSlime* this = (EnSlime*)thisx;
+    s32 reviveTimeSeconds;
+    float Distance = 30000.0f;
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    u8 baseHealth = this->actor.colChkInfo.health;
+
+    switch (Difficulty) {
+    case 1: {
+        Distance = 60000.0f;
+        reviveTimeSeconds = 10;
+        this->reviveTime = (reviveTimeSeconds * 10) + 100;
+        this->actor.colChkInfo.health = baseHealth * 2;
+        break;
+    }
+
+    case 2: {
+        Distance = 90000.0f;
+        reviveTimeSeconds = 5;
+        this->reviveTime = (reviveTimeSeconds * 5);
+        this->actor.colChkInfo.health = baseHealth * 5;
+        break;
+    }
+
+    default: {
+        Distance = 30000.0f;
+        this->actor.colChkInfo.health = baseHealth;
+        break;
+    }
+    }
+
+    if (this->actor.shape.rot.x <= 0) {
+        this->distLimit = Distance;
+    }
+    else {
+        this->distLimit = this->actor.shape.rot.x * 40.0f;
+    }
+}
+
+RECOMP_HOOK("EnSlime_SetupRevive") void ReviveBuff(EnSlime* this) {
+
+    CanUseTimer = true;
+
+}
+
+
+
+RECOMP_HOOK("EnSlime_Update") void ReviveBuffFix(EnSlime* this) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    u8 baseHealth = this->actor.colChkInfo.health;
+    f32 sinFactor;
+
+    if (CanUseTimer) {
+
+        Timer++;
+
+        if (Timer >= 5) {
+
+            switch (Difficulty) {
+            case 1:
+                this->actor.colChkInfo.health = baseHealth * 2;
+                break;
+
+            case 2:
+                this->actor.colChkInfo.health = baseHealth * 5;
+                break;
+
+            default:
+                break;
+            }
+            Timer = 0;
+            CanUseTimer = false;
+        }
+    }
+
+    if (Difficulty == 2) {
+
+        if (Rand_ZeroOne() < 0.05f) {
+
+            this->actor.home.pos.x =
+                this->actor.world.pos.x + Rand_CenteredFloat(300.0f);
+
+            this->actor.home.pos.z =
+                this->actor.world.pos.z + Rand_CenteredFloat(300.0f);
+
+            this->actor.home.pos.y =
+                this->actor.world.pos.y + Rand_CenteredFloat(50.0f);
+        }
+    }
+}
+
+RECOMP_HOOK_RETURN("EnSlime_MoveInDirection") void SlimeMovement(EnSlime* this, PlayState* play) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    f32 sinFactor;
+
+    sinFactor = fabsf(Math_SinF(this->timer * (M_PIf / 24)));
+    switch (Difficulty) {
+    case 1:
+        this->actor.speed = (0.8f * sinFactor) + 0.2f * 1.5;
+        break;
+
+    case 2:
+        this->actor.speed = (0.8f * sinFactor) + 0.2f * 3;
+        break;
+
+    default:
+        break;
+    }
+
+}
+
+RECOMP_PATCH void EnSlime_SetupJump(EnSlime* this) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    float dist = this->actor.xzDistToPlayer;
+    float Multiplier = 1;
+
+    switch (Difficulty) {
+    case 1:
+        Multiplier = 2;
+        break;
+
+    case 2:
+        Multiplier = 3;
+        break;
+
+    default:
+        Multiplier = 1;
+        break;
+    }
+
+    if (dist < 60.0f) {
+        this->actor.velocity.y = 6.0f;
+        this->actor.speed = 2.5f * Multiplier / 2;
+        this->actor.gravity = -1.0f;
+    }
+    else if (dist < 120.0f) {
+        this->actor.velocity.y = 11.0f;
+        this->actor.speed = 4.5f * Multiplier / 2;
+        this->actor.gravity = -2.0f;
+    }
+    else {
+        this->actor.velocity.y = 18.0f;
+        this->actor.speed = 7.0f * Multiplier / 2;
+        this->actor.gravity = -3.5f;
+    }
+    this->actor.world.rot.y = this->actor.shape.rot.y;
+    this->timer = 12;
+    this->eyeTexIndex = EN_SLIME_EYETEX_OPEN;
+    Math_StepToF(&this->actor.scale.x, 0.008f, 0.0025f);
+    Math_StepToF(&this->actor.scale.y, 0.011f, 0.0025f);
+    this->actor.scale.z = this->actor.scale.x;
+    Actor_PlaySfx(&this->actor, NA_SE_EN_SLIME_JUMP);
+    this->actionFunc = EnSlime_Jump;
+}
+
+RECOMP_HOOK("EnSlime_Idle") void Sight(EnSlime* this, PlayState* play) {
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+    float Distance = 280.0f;
+
+    switch (Difficulty) {
+    case 1:
+        Distance = 480.f;
+        break;
+
+    case 2:
+        Distance = 690.f;
+        break;
+
+    default:
+        Distance = 280.0f;
+        break;
+    }
+
+    if ((Player_GetMask(play) != PLAYER_MASK_STONE) && (this->actor.xzDistToPlayer < Distance) &&
+        Actor_IsFacingPlayer(&this->actor, 0x5000)) {
+        EnSlime_SetupTurnToPlayer(this);
+    }
+    else if (this->timer == 0) {
+        // Start moving (remaining idle)
+        EnSlime_SetupMoveInDirection(this);
+    }
+
+}
