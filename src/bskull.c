7369d2674886d08bca7fb4dbef605ee6f29b520d
TSEG <pcat2122@gmail.com>
2026-05-16 13:13:05 -0400

    Skulltula now turns more frequently but is turned around for less time

--- src/bskull.c
+++ src/bskull.c
@@ -19,22 +19,25 @@ RECOMP_PATCH s16 func_808A5BEC(EnSt* this) {
         ret = this->actor.yawTowardsPlayer;
 
         if (DECR(this->unk_30E) == 0) {
-            float turnAwayChance = 0.25f;
+            float turnAwayChance = 0.45f;
+            int faceAwayDuration = 30;
 
             if (this->unk_18C & 2) {
                 this->unk_18C &= ~2;
 
                 Actor_PlaySfx(&this->actor, NA_SE_EN_STALTU_ROLL);
+                this->unk_30E = 30;
             }
             else {
-
                 switch (Difficulty) {
                 case 0:
-                    turnAwayChance = 0.33f;
+                    turnAwayChance = 0.9f;
+                    faceAwayDuration = 30;
                     break;
 
                 case 1:
-                    turnAwayChance = 0.10f;
+                    turnAwayChance = 0.70f;
+                    faceAwayDuration = 10;
                     break;
                 }
 
@@ -42,22 +45,20 @@ RECOMP_PATCH s16 func_808A5BEC(EnSt* this) {
                     this->unk_18C |= 2;
 
                     Actor_PlaySfx(&this->actor, NA_SE_EN_STALTU_ROLL);
+                    this->unk_30E = faceAwayDuration;
+                }
+                else {
+                    this->unk_30E = 30;
                 }
             }
-
             this->unk_310 = 8;
-
             if (this->unk_18C & 1) {
                 this->unk_310 *= 2;
             }
-
-            this->unk_30E = 30;
         }
-
         if (this->unk_18C & 2) {
             ret = BINANG_ROT180(ret);
         }
     }
-
     return ret;
 }
