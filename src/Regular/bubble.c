1f75999191137bd70949972bffa76f346982c293
TSEG <pcat2122@gmail.com>
2026-05-29 19:39:56 -0400

    Overhaul Bubbles
    Included header because I did changes to it

--- src/Regular/bubble.c
+++ src/Regular/bubble.c
@@ -12,6 +12,61 @@ void EnBb_SetupFlyIdle(EnBb* this);
 void EnBb_Attack(EnBb* this, PlayState* play);
 void EnBb_Down(EnBb* this, PlayState* play);
 
+extern s32 EnBb_OverrideLimbDraw(PlayState*, s32, Gfx**, Vec3f*, Vec3s*, Actor*);
+extern void EnBb_PostLimbDraw(PlayState*, s32, Gfx**, Vec3s*, Actor*);
+extern void EnBb_SetupDown(EnBb*);
+extern void EnBb_SetupDamage(EnBb*);
+extern void EnBb_SetupDead(EnBb*, PlayState*);
+extern void EnBb_SetupFrozen(EnBb*);
+extern void EnBb_Freeze(EnBb*);
+extern void EnBb_Thaw(EnBb*, PlayState*);
+
+typedef enum {
+    /* 0x0 */ EN_BB_DMGEFF_NONE,
+    /* 0x1 */ EN_BB_DMGEFF_STUN,
+    /* 0x3 */ EN_BB_DMGEFF_ICE_ARROW = 0x3,
+    /* 0x4 */ EN_BB_DMGEFF_LIGHT_ARROW,
+    /* 0x5 */ EN_BB_DMGEFF_ZORA_MAGIC,
+    /* 0xE */ EN_BB_DMGEFF_HOOKSHOT = 0xE
+} EnBbDamageEffect;
+
+static DamageTable sDamageTable = {
+    /* Deku Nut       */ DMG_ENTRY(0, EN_BB_DMGEFF_STUN),
+    /* Deku Stick     */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Horse trample  */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Explosives     */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Zora boomerang */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Normal arrow   */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* UNK_DMG_0x06   */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Hookshot       */ DMG_ENTRY(0, EN_BB_DMGEFF_HOOKSHOT),
+    /* Goron punch    */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Sword          */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Goron pound    */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Fire arrow     */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Ice arrow      */ DMG_ENTRY(2, EN_BB_DMGEFF_ICE_ARROW),
+    /* Light arrow    */ DMG_ENTRY(2, EN_BB_DMGEFF_LIGHT_ARROW),
+    /* Goron spikes   */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Deku spin      */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Deku bubble    */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Deku launch    */ DMG_ENTRY(2, EN_BB_DMGEFF_NONE),
+    /* UNK_DMG_0x12   */ DMG_ENTRY(0, EN_BB_DMGEFF_STUN),
+    /* Zora barrier   */ DMG_ENTRY(0, EN_BB_DMGEFF_ZORA_MAGIC),
+    /* Normal shield  */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Light ray      */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Thrown object  */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Zora punch     */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Spin attack    */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+    /* Sword beam     */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Normal Roll    */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* UNK_DMG_0x1B   */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* UNK_DMG_0x1C   */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Unblockable    */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* UNK_DMG_0x1E   */ DMG_ENTRY(0, EN_BB_DMGEFF_NONE),
+    /* Powder Keg     */ DMG_ENTRY(1, EN_BB_DMGEFF_NONE),
+};
+
+void func_80833B18(PlayState* play, Player* this, s32 arg2, f32 speed, f32 velocityY, s16 arg5, s32 invincibilityTimer);
+
 void EnBb_CheckForWall(EnBb* this) {
     s16 yawDiff;
 
@@ -60,7 +115,107 @@ RECOMP_HOOK_RETURN("EnBb_Init") void BBBuff(Actor* thisx, PlayState* play) {
     default:
         break;
     }
+}
 
+RECOMP_HOOK("EnBb_Init") void SetType(Actor* thisx, PlayState* play) {
+
+    EnBb* this = (EnBb*)thisx;
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+
+    if (Difficulty == 1) {
+
+        if (this->actor.home.rot.x == 0) {
+            if (Difficulty == 1) {
+                this->actor.home.rot.z = (Rand_ZeroOne() * 3.0f);
+            }
+            else {
+                this->actor.home.rot.z = 0;
+            }
+
+            this->variant = this->actor.home.rot.z;
+
+            this->actor.home.rot.x = 1;
+        }
+    }
+
+}
+
+RECOMP_PATCH void EnBb_UpdateDamage(EnBb* this, PlayState* play) {
+    if (this->collider.base.acFlags & AC_HIT) {
+        this->collider.base.acFlags &= ~AC_HIT;
+        this->collider.base.atFlags &= ~(AT_HIT | AT_BOUNCED);
+        this->collider.base.atFlags &= ~AT_ON;
+        if ((this->drawDmgEffType != ACTOR_DRAW_DMGEFF_FROZEN_NO_SFX) ||
+            !(this->collider.elem.acHitElem->atDmgInfo.dmgFlags & 0xDB0B3)) {
+            Actor_SetDropFlag(&this->actor, &this->collider.elem);
+            this->flameScaleY = 0.0f;
+            this->flameScaleX = 0.0f;
+            EnBb_Thaw(this, play);
+
+            if (Actor_ApplyDamage(&this->actor) == 0) {
+                Enemy_StartFinishingBlow(play, &this->actor);
+            }
+
+            if (this->actor.colChkInfo.damageEffect == EN_BB_DMGEFF_ICE_ARROW) {
+                EnBb_Freeze(this);
+                if (this->actor.colChkInfo.health == 0) {
+                    this->timer = 3;
+                    this->collider.base.acFlags &= ~AC_ON;
+                }
+
+                EnBb_SetupFrozen(this);
+            }
+            else if (this->actor.colChkInfo.health == 0) {
+                EnBb_SetupDead(this, play);
+            }
+            else {
+                EnBb_SetupDamage(this);
+            }
+
+            if (this->actor.colChkInfo.damageEffect == EN_BB_DMGEFF_LIGHT_ARROW) {
+                this->drawDmgEffAlpha = 4.0f;
+                this->drawDmgEffScale = 0.4f;
+                this->drawDmgEffType = ACTOR_DRAW_DMGEFF_LIGHT_ORBS;
+                Actor_Spawn(&play->actorCtx, play, ACTOR_EN_CLEAR_TAG, this->collider.elem.acDmgInfo.hitPos.x,
+                    this->collider.elem.acDmgInfo.hitPos.y, this->collider.elem.acDmgInfo.hitPos.z, 0, 0, 0,
+                    CLEAR_TAG_PARAMS(CLEAR_TAG_SMALL_LIGHT_RAYS));
+            }
+        }
+    }
+    else if (this->collider.base.atFlags & AT_BOUNCED) {
+        this->collider.base.atFlags &= ~(AT_HIT | AT_BOUNCED);
+        if (this->actionFunc != EnBb_Down) {
+            this->actor.world.rot.y = this->actor.yawTowardsPlayer + 0x8000;
+            this->actor.shape.rot.y = this->actor.world.rot.y;
+            EnBb_SetupDown(this);
+        }
+    }
+    else if (this->collider.base.atFlags & AT_HIT) {
+        this->collider.base.atFlags &= ~AT_HIT;
+        this->actor.world.rot.y = this->actor.yawTowardsPlayer + 0x8000;
+        this->actor.shape.rot.y = this->actor.world.rot.y;
+        Actor_PlaySfx(&this->actor, NA_SE_EN_BUBLE_BITE);
+
+        if (this->flameScaleX > 0.0f) {
+            Player* player = GET_PLAYER(play);
+            switch (this->variant) {
+            case 1:
+                func_80833B18(play, player, 3, 0.0f, 0.0f, 0, 20);
+                break;
+            case 2:
+                func_80833B18(play, player, 4, 0.0f, 0.0f, 0, 20);
+                break;
+            case 0:
+            default:
+                gSaveContext.jinxTimer = 1200;
+                break;
+            }
+        }
+
+        if (this->actionFunc == EnBb_Attack) {
+            EnBb_SetupFlyIdle(this);
+        }
+    }
 }
 
 RECOMP_HOOK("EnBb_Update") void Updating(Actor* thisx, PlayState* play) {
@@ -260,4 +415,60 @@ RECOMP_PATCH void EnBb_FlyIdle(EnBb* this, PlayState* play) {
     else if (this->timer == 0) {
         if (Difficulty == 0) EnBb_SetupFlyIdle(this);
     }
+}
+
+extern Gfx gEffFire1DL[];
+
+RECOMP_PATCH void EnBb_Draw(Actor* thisx, PlayState* play) {
+    s32 pad;
+    EnBb* this = (EnBb*)thisx;
+    MtxF* currentMatrixState;
+    Gfx* gfx;
+
+    OPEN_DISPS(play->state.gfxCtx);
+
+    int Difficulty = (int)recomp_get_config_double("diff_option");
+
+    gfx = POLY_OPA_DISP;
+    gSPDisplayList(&gfx[0], gSetupDLs[SETUPDL_25]);
+    POLY_OPA_DISP = &gfx[1];
+    SkelAnime_DrawOpa(play, this->skelAnime.skeleton, this->skelAnime.jointTable, EnBb_OverrideLimbDraw,
+        EnBb_PostLimbDraw, &this->actor);
+
+    if (this->flameScaleX > 0.0f) {
+        currentMatrixState = Matrix_GetCurrent();
+        Gfx_SetupDL25_Xlu(play->state.gfxCtx);
+        Matrix_RotateYS(((Camera_GetCamDirYaw(GET_ACTIVE_CAM(play)) - this->actor.shape.rot.y) + 0x8000),
+            MTXMODE_APPLY);
+        Matrix_Scale(this->flameScaleX, this->flameScaleY, 1.0f, MTXMODE_APPLY);
+        gDPSetPrimColor(POLY_XLU_DISP++, 0x80, 0x80, 255, 255, 255, 255);
+        if (Difficulty == 1) {
+            switch (this->variant) {
+            case 1:
+                gDPSetEnvColor(POLY_XLU_DISP++, 0, 100, 255, 255);
+                break;
+            case 2:
+                gDPSetEnvColor(POLY_XLU_DISP++, 255, 255, 0, 255);
+                break;
+            case 0:
+            default:
+                gDPSetEnvColor(POLY_XLU_DISP++, 255, 0, 255, 64);
+                break;
+            }
+        }
+        else {
+            gDPSetEnvColor(POLY_XLU_DISP++, 0, 0, 255, 0);
+        }
+        gSPSegment(
+            POLY_XLU_DISP++, 0x08,
+            Gfx_TwoTexScroll(play->state.gfxCtx, 0, 0, 0, 32, 64, 1, 0, (play->gameplayFrames * -20) & 0x1FF, 32, 128));
+        currentMatrixState->mf[3][1] -= 47.0f * this->flameScaleY;
+        MATRIX_FINALIZE_AND_LOAD(POLY_XLU_DISP++, play->state.gfxCtx);
+        gSPDisplayList(POLY_XLU_DISP++, gEffFire1DL);
+    }
+
+    Actor_DrawDamageEffects(play, &this->actor, this->bodyPartsPos, BUBBLE_BODYPART_MAX, this->drawDmgEffScale,
+        this->drawDmgEffFrozenSteamScale, this->drawDmgEffAlpha, this->drawDmgEffType);
+
+    CLOSE_DISPS(play->state.gfxCtx);
 }
