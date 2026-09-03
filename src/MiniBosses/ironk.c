45f176b860d58c0c054357371e045e687ca9b8de
TSEG <pcat2122@gmail.com>
2026-09-02 22:32:37 -0400

    Forgot about Iron Knuckle

--- src/MiniBosses/ironk.c
+++ src/MiniBosses/ironk.c
@@ -449,7 +449,9 @@ RECOMP_HOOK("EnIk_Update") void IKUpdate(Actor* thisx, PlayState* play2) {
 
     switch (Difficulty) {
     case 0:
-        this->actor.colChkInfo.damage = (this->actor.colChkInfo.damage) / 2;
+        if (this->actor.colChkInfo.damage != 1) {
+            this->actor.colChkInfo.damage = (this->actor.colChkInfo.damage) / 2;
+        }
         break;
 
     case 1:
